98e931ed62783b7bdffef9fd4a7b45bf06a1f812
bu-younes <alg7afri.m@gmail.com>
2023-05-19 11:25:09 +0400

    create home page

---
+++ home.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tec Mahindra System</title>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			margin: 0;
+			padding: 0;
+		}
+
+		h1, h2, h3 {
+			margin: 0;
+		}
+
+		a {
+			text-decoration: none;
+			color: #333;
+		}
+
+		header {
+			background-color: #333;
+			color: #fff;
+			padding: 10px;
+		}
+
+		nav ul {
+			margin: 0;
+			padding: 0;
+			list-style: none;
+		}
+
+		nav li {
+			display: inline-block;
+			margin: 0 10px;
+		}
+
+		nav a {
+			color: #fff;
+		}
+
+		.banner {
+			background-image: url('https://cdn.pixabay.com/photo/2017/08/07/19/44/book-2608889_1280.jpg');
+			background-size: cover;
+			background-position: center;
+			color: #fff;
+			text-align: center;
+			padding: 100px 0;
+		}
+
+		.btn {
+			background-color: #fff;
+			color: #333;
+			padding: 10px 20px;
+			border-radius: 5px;
+		}
+
+		.services {
+			padding: 50px;
+			background-color: #f2f2f2;
+		}
+
+		.service {
+			background-color: #fff;
+			padding: 20px;
+			border-radius: 5px;
+			margin-bottom: 20px;
+		}
+
+		.about {
+			background-color: #333;
+			color: #fff;
+			padding: 50px;
+			text-align: center;
+		}
+
+		footer {
+			background-color: #ccc;
+			color: #333;
+			padding: 10px;
+			text-align: center;
+		}
+
+	</style>
+</head>
+<body>
+	<header>
+		<h1>Tec Mahindra System</h1>
+		<nav>
+			<ul>
+				<li><a href="home.php">Home</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="contact.php">Contact</a></li>
+				<li><a href="register.php">Register.</a></li>
+                <li><a href="login.php">Login</a></li>
+
+			</ul>
+		</nav>
+	</header>
+	
+	<main>
+		<section class="banner">
+			<h2 class="btn">Welcome to Tec Mahindra System</h2>
+			<a href="#" class="btn">Get Started</a>
+		</section>
+		
+		<section class="services">
+			<h2>Our Services</h2>
+			<div class="service">
+				<h3>Time Sheet</h3>
+				<p>...................................</p>
+			</div>
+			<div class="service">
+				<h3>...........</h3>
+				<p>....................................</p>
+			</div>
+			<div class="service">
+				<h3>............</h3>
+				<p>....................................</p>
+			</div>
+		</section>
+		
+		<section class="about">
+			<h2>About Us</h2>
+			<p>................................................................................
+            ................................................................................				 
+            ................................................................................				   
+			.................................................................................
+            ................................................................................</p>
+</section>
+</main>
+<footer>
+	<p>&copy; 2023 Tec Mahindra System. All rights reserved.</p>
+</footer>
+</body>
+</html>
